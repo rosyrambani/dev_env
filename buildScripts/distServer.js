689692fe1edf943d50495b04cea75d2eda829645
Unknown <rosys9319@gmail.com>
2018-07-20 15:02:11 -0400

    dev env completed

--- buildScripts/distServer.js
+++ buildScripts/distServer.js
@@ -1,22 +1,18 @@
 import express from 'express';
 import path from 'path';
 import open from 'open';
-import webpack from 'webpack';
-import config from '../webpack.config.dev'
+import compresssion from 'compression';
 
 /* eslint-disable no-console */
 
 const port = 3000;
 const app = express();
-const compiler = webpack(config);
 
-app.use(require('webpack-dev-middleware')(compiler, {
-  noInfo: true,
-  publicPath: config.output.publicPath
-}));
+app.use(compresssion()); // for gzip compression
+app.use(express.static('dist'));
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '../src/index.html'));
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
 app.get('/users', function(req, res) {
